2efd7969b5c3a4ca77284e5e0c4086bd42e643ee
Nattapon10545 <nattapon10545@gmail.com>
2025-04-26 23:34:20 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -9,7 +9,12 @@ loginForm.addEventListener('submit', async (e) => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   
-  const res = await fetch(`https://script.google.com/macros/s/AKfycbygOLq7mNYXASGxbhpj6Xrvmv9StRLvrgT-_kNI6uyLXF-7S5EzO08tTD0F-9C7v6vkbg/exec?action=login&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
+  const res = await fetch('https://script.google.com/macros/s/AKfycbygOLq7mNYXASGxbhpj6Xrvmv9StRLvrgT-_kNI6uyLXF-7S5EzO08tTD0F-9C7v6vkbg/exec?action=login', {
+  method: 'POST',  // <-- ต้องเป็น POST นะ
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ email, password }) // <-- body ต้องอยู่ตรงนี้
+});
+
 
   
   const data = await res.json();
